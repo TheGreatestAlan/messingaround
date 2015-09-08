d549c3fabcbc1908010460218eb9e31ba1cb91ae
TheGreatestAlan <alan.hoang.nguyen@gmail.com>
2015-09-07 18:43:20 -0600

    index.js

--- nodestuff/led/routes/index.js
+++ nodestuff/led/routes/index.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
-///* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-//});
-
+var alicia = 'Hello.  Alicia. You\'re one of the perviest people I\'ve ever met .';
+var drew = 'Hello. Drew.  You silly old man.';
+var standard = 'THE WORLD IS MIIIIINNNEEEEEE!!!!!!';
+var me = 'Why hello there, you handsome devil';
 
 router.get('/',function(req, res) {
+	if(req.connection.remoteAddress == '50.183.124.90'){
+		standard = alicia;
+	} else if (req.connection.remoteAddress == '66.249.84.199') {
+		standard = drew;
+	} else if (req.connection.remoteAddress == '66.87.151.233') {
+		standard = drew;
+	} else if (req.connection.remoteAddress == '107.2.238.40') {
+		standard = me;
+	}
 	res.render('index', {
-		title: 'My App!'
+		title: 'ALANALANALAN',
+		inside: standard
 	});
+	console.log(req.connection.remoteAddress);
 	var util = require('util'),
 	    exec = require('child_process').exec,
 	    child;
